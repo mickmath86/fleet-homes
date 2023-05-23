25c1508714ad97c79ba9ad35d354b5e67a1e6b02
Michael Mathias <mickmath86@gmail.com>
2023-05-23 15:52:08 -0700

    new modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
-import Hero from './Hero'
+import Hero from './components/Hero'
+import Steps from './components/Steps'
+import Mission from './components/Mission'
+import ProductRight from './components/ProductRight'
+import ProductLeft from './components/ProductLeft'
 
 export default function Home() {
   return (
     <>  
       <Hero/>
+      <Steps/>
+      <Mission/>
+      <ProductRight/>
+      <ProductLeft/>
     </>
   )
 }
