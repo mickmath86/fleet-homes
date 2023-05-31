1008de79a0fcbf472b14dc32b2881a0162dde1a9
Michael Mathias <mickmath86@gmail.com>
2023-05-30 22:50:19 -0700

    google tag manager

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Mission from './components/Mission'
 import ProductRight from './components/ProductRight'
 import ProductLeft from './components/ProductLeft'
 import Footer from './components/Footer'
+import Script from 'next/script'
+
 
 export default function Home() {
   return (
@@ -15,6 +17,22 @@ export default function Home() {
       <ProductRight/>
       <ProductLeft/>
       <Footer/> 
+          <div className="container">
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=GTM-MCWRCKQ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'GTM-MCWRCKQ');
+            `}
+          </Script>
+        </div>
     </>
   )
 }
